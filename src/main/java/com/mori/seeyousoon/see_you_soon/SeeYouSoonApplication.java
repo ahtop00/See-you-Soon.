2f0695a9c61acc6e688ae48f20766509c791408a
KYS <ehcl1027@gmail.com>
2025-02-13 19:57:10 +0900

    Initial commit

---
+++ src/main/java/com/mori/seeyousoon/see_you_soon/SeeYouSoonApplication.java
@@ -0,0 +1,13 @@
+package com.mori.seeyousoon.see_you_soon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeYouSoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeYouSoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mori/seeyousoon/see_you_soon/SeeYouSoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mori.seeyousoon.see_you_soon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeYouSoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
